bf321c353414c296e00732bd6c1abb40006b9eac
v_gangzhang <v_gangzhang@tencent.com>
2015-01-15 14:15:34 +0800

    合并主干，更新apollo组件

---
+++ COMMLIB/DbConnectionString.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TENCENT.OSS.CFT.KF.DataAccess;
+using CFT.Apollo.Data.ConnectionString;
+
+namespace CommLib
+{
+    public class DbConnectionString
+    {
+
+        #region  单例
+        private static DbConnectionString _instance;
+
+        private static readonly object _locker = new object();
+
+        public static DbConnectionString Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_locker)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DbConnectionString();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        private DbConnectionString()
+        {
+        }
+        #endregion
+
+        /// <summary>
+        /// 获取连接对象
+        /// </summary>
+        /// <param name="dataSourceName">数据库别名</param>
+        /// <returns></returns>
+        public MySqlAccess GetConnection(string dataSourceName)
+        {
+            var connectionString = GetConnectionString(dataSourceName);
+            return new MySqlAccess(connectionString);
+        }
+
+        /// <summary>
+        /// 获取连接字符串
+        /// </summary>
+        /// <param name="dataSourceName">数据库别名</param>
+        /// <returns></returns>
+        public string GetConnectionString(string dataSourceName)
+        {
+            if (string.IsNullOrEmpty(dataSourceName))
+            {
+                throw new ArgumentNullException("dataSourceName");
+            }
+
+            var connectionString = ConnectionStringHelper.Get(dataSourceName);
+
+            if (connectionString == null)
+            {
+                throw new Exception(string.Format("找不到名为\"{0}\"的连接字符串", dataSourceName));
+            }
+
+            return connectionString;
+        }
+    }
+}
---
+++ COMMLIB/DbNameAnalysis.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace commLib
+{
+    public class DbNameAnalysis
+    {
+
+        #region 单例
+        private static DbNameAnalysis _instance = null;
+
+        private static readonly object _locker = new object();
+
+        public static DbNameAnalysis Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_locker)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DbNameAnalysis();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private DbNameAnalysis()
+        { }
+        #endregion
+
+
+        /// <summary>
+        /// 返回拆分之后的库名
+        /// </summary>
+        /// <param name="strDbName">s数据库前缀</param>
+        ///         /// <param name="uid"></param>
+        /// <returns></returns>
+        public string GetDbnameByUid(string strDbName, string uid)
+        {
+            if (uid.Length < 2)
+            {
+                throw new ArgumentException("参数异常：Uid必须大于两位");
+            }
+
+            var index = int.Parse(uid.Substring(uid.Length - 2, 2));
+            var step = 3;
+            for (int i = 0; i <= 99; i++)
+            {
+                if (index >= i && index <= i + step)
+                {
+                    var min = i.ToString();
+                    var max = (i + step).ToString();
+                    if (i < 10)
+                    {
+                        min = "0" + i;
+                    }
+                    if (int.Parse(max) < 10)
+                    {
+                        max = "0" + max;
+                    }
+                    return strDbName += "(" + min + "-" + max + ")";
+                }
+                i += step;
+            }
+
+            return "";
+        }
+
+    }
+}
