58d2f5536f11c8fbb1f95afa8b559aea4b972f0e
v_yqyqguo <v_yqyqguo@tencent.com>
2015-02-12 10:08:53 +0800

    客服系统接口—退款登记

---
+++ src/CFT.CSOMS.DAL/InternetBank/InternetBankData.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Configuration;
+using TENCENT.OSS.CFT.KF.DataAccess;
+using TENCENT.OSS.CFT.KF.Common;
+using System.Data;
+using System.Collections;
+using System.IO;
+using System.Web.Mail;
+
+using TENCENT.OSS.C2C.Finance.DataAccess;
+using TENCENT.OSS.C2C.Finance.Common;
+using TENCENT.OSS.C2C.Finance.Common.CommLib;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Xml;
+using System.Text.RegularExpressions;
+using CFT.CSOMS.DAL.Infrastructure;
+
+namespace CFT.CSOMS.DAL.InternetBank
+{
+    public  class InternetBankData
+    {
+
+        public void AddRefundInfo( string FOrderId ,int FRefund_type,string FSam_no ,string FRecycle_user,string FSubmit_user , string FRefund_amount)
+        {
+            string msg;
+            var da = MySQLAccessFactory.GetMySQLAccess("DataSource_ht");
+            try
+            {
+                string FCoding = "";
+                string FAmount = ""; //交易金额
+                int FTrade_state = 2;
+                string FBuy_acc = "";
+                string FTrade_desc = "";//交易说明
+                string FMemo = "";//备注
+
+                int FRefund_state = 1; //退款状态
+                int FSubmit_refund = 3; //提交退款状态
+
+                da.OpenConn();
+                string strSql = "select " + GeRefundInfoFields() + " from c2c_fmdb.t_refund_info where Forder_id='" + FOrderId + "'";
+                DataTable dt = da.GetTable(strSql);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    //msg = "添加失败:财付通订单号已存在" + FOrderId;
+                    throw new LogicException("财付通订单号已存在:" + FOrderId);
+                }
+
+                string striceWhere = "listid=" + FOrderId;
+                DataTable dt_ice = CommQuery.GetTableFromICE(striceWhere, "query_order_service", out msg);
+                if (dt_ice != null && dt_ice.Rows.Count > 0)
+                {
+                    object obj = dt_ice.Rows[0]["Fcoding"];
+                    if (obj != null) FCoding = obj.ToString().Trim();
+                    obj = dt_ice.Rows[0]["Fpaynum"];
+                    if (obj != null) FAmount = obj.ToString().Trim();
+                    obj = dt_ice.Rows[0]["Ftrade_state"];
+                    if (obj != null)
+                    {
+                        FTrade_state = int.Parse(obj.ToString().Trim());
+                        if (FTrade_state == 2)
+                        {
+                            FSubmit_refund = 2;
+                        }
+                    }
+                    obj = dt_ice.Rows[0]["Fbuyid"];
+                    if (obj != null) FBuy_acc = obj.ToString().Trim();
+                    obj = dt_ice.Rows[0]["Fmemo"];
+                    if (obj != null) FTrade_desc = obj.ToString().Trim();
+                }
+                else
+                {
+                    //msg = "通用查询订单号不存在：" + FOrderId;
+                    throw new LogicException("订单号不存在：" + FOrderId + msg);
+                }
+                //判断退款金额<=订单金额
+                int oAmount = Convert.ToInt32(FAmount);
+                int rAmount = 0;
+                if (!string.IsNullOrEmpty(FRefund_amount))
+                {
+                    rAmount = Convert.ToInt32(FRefund_amount);
+                }
+                if (rAmount <= oAmount)
+                {
+                    //退款金额<=订单金额
+                    strSql = "insert into c2c_fmdb.t_refund_info(Forder_id,Fcoding,Famount,Ftrade_state,Fbuy_acc,Ftrade_desc,Frefund_type,Frefund_state,Fmemo,Fsubmit_user,Frecycle_user,Fsam_no,Fsubmit_refund,Frefund_amount,Fcreate_time,Fmodify_time) values('{0}','{1}','{2}',{3},'{4}','{5}',{6},{7},'{8}','{9}','{10}','{11}',{12},{13},now(),now())";
+                    strSql = String.Format(strSql, FOrderId, FCoding, FAmount, FTrade_state, FBuy_acc, FTrade_desc, FRefund_type, FRefund_state, FMemo, FSubmit_user, FRecycle_user, FSam_no, FSubmit_refund, FRefund_amount);
+
+                    da.ExecSqlNum(strSql);
+                }
+                else
+                {
+                    throw new LogicException("退款金额" + rAmount + "大于订单金额" + oAmount);
+                }
+            }
+            catch (LogicException err)
+            {
+                throw new LogicException(err.Message);
+            }
+            finally
+            {
+                da.Dispose();
+            }
+        }
+
+        private static string GeRefundInfoFields()
+        {
+            return " Fid,Forder_id,Fcoding,Famount,Ftrade_state,Fbuy_acc,Ftrade_desc,Frefund_type,Frefund_state,Fmemo,Fcreate_time,Fmodify_time,Fsubmit_user,Frecycle_user,Fsam_no,Fsubmit_refund,Frefund_amount ";
+        }
+    }
+}
---
+++ src/KF_SERVICE/CSAPI/TradeService.asmx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using System.Xml.Serialization;
+using CFT.CSOMS.Service.CSAPI.Utility;
+using System.IO;
+using CFT.CSOMS.Service.CSAPI.Language;
+using CFT.CSOMS.Service.CSAPI.PayMent;
+using System.Configuration;
+using System.Net;
+using CFT.CSOMS.COMMLIB;
+namespace CSAPI
+{
+  /// <summary>
+    /// Summary description for WechatService
+    /// </summary>
+    [WebService(Namespace = "")]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class TradeService : System.Web.Services.WebService
+    {
+        #region 网银
+        [WebMethod]
+        public void  AddRefundInfo()
+        {
+            try
+            {
+                string msg;
+
+                string FOrderId = ""; //财付通订单号
+                int FRefund_type = 1; //退款类型
+                string FSam_no = "";//SAM工单号
+                string FRecycle_user = "";//回收人
+                string FSubmit_user = ""; //登记人
+                string FRefund_amount = "0"; //退款金额
+
+                Dictionary<string, string> paramsHt = APIUtil.GetQueryStrings();
+
+                //必填参数验证
+                APIUtil.ValidateParamsNew(paramsHt, "appid", "FOrderId", "token");
+                //token验证
+                APIUtil.ValidateToken(paramsHt);
+              
+                if (paramsHt.Keys.Contains("FOrderId")) 
+                {
+                    FOrderId = paramsHt["FOrderId"].ToString(); 
+                }
+                if (paramsHt.Keys.Contains("FRefund_type"))
+                {
+                    FRefund_type = int.Parse(paramsHt["FRefund_type"].ToString());
+                }
+                if (paramsHt.Keys.Contains("FSam_no"))
+                {
+                    FSam_no = paramsHt["FSam_no"].ToString();
+                }
+                if (paramsHt.Keys.Contains("FRecycle_user"))
+                {
+                    FRecycle_user = paramsHt["FRecycle_user"].ToString();
+                }
+                if (paramsHt.Keys.Contains("FRefund_amount"))
+                {
+                    FRefund_amount = paramsHt["FRefund_amount"].ToString();
+                }
+                string infos = "";
+                try
+                {
+                    new CFT.CSOMS.BLL.InternetBank.InternetBankService().AddRefundInfo(FOrderId, FRefund_type, FSam_no, FRecycle_user, FSubmit_user, FRefund_amount);
+                }
+                catch (Exception e)
+                {
+                    infos = e.Message;
+                }
+                Record record = new Record();
+                record.RetValue = infos.ToLower();
+                List<Record> list = new List<Record>();
+                list.Add(record);
+
+                APIUtil.Print<Record>(list);
+            }
+            catch (ServiceException se)
+            {
+                SunLibrary.LoggerFactory.Get("AddRefundInfo").ErrorFormat("return_code:{0},msg:{1}", se.GetRetcode, se.GetRetmsg);
+                APIUtil.PrintError(se.GetRetcode, se.GetRetmsg);
+            }
+            catch (Exception ex)
+            {
+                SunLibrary.LoggerFactory.Get("AddRefundInfo").ErrorFormat("return_code:{0},msg:{1}", APIUtil.ERR_SYSTEM, ex.Message);
+                APIUtil.PrintError(APIUtil.ERR_SYSTEM, ErroMessage.MESSAGE_ERROBUSINESS);
+            }
+        }
+        #endregion
+    }
+}
