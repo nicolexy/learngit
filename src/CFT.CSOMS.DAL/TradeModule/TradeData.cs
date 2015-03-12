d9ae2ea6f0791b5117faf80a21eeffc4e4fe31a4
darrenran <darrenran@tencent.com>
2015-03-12 16:08:25 +0800

    1.恢复一点通的页面重构 2.写交易记录查询接口

--- src/CFT.CSOMS.DAL/TradeModule/TradeData.cs
+++ src/CFT.CSOMS.DAL/TradeModule/TradeData.cs
@@ -8,6 +8,7 @@ using TENCENT.OSS.C2C.Finance.Common.CommLib;
 using CFT.CSOMS.DAL.Infrastructure;
 using CFT.CSOMS.DAL.CFTAccount;
 using CommLib;
+using System.Configuration;
 
 namespace CFT.CSOMS.DAL.TradeModule
 {
@@ -81,6 +82,258 @@ namespace CFT.CSOMS.DAL.TradeModule
                 string Sql = "Select * from  c2c_fmdb.t_log_ConFinRemove where Fuin='" + qqid + "' Order by FmodifyTime DESC";
                 DataSet ds = da.dsGetTotalData(Sql);
                 return ds;
+            }
+        }
+
+        #region 交易记录查询
+
+        public DataSet GetTradeList(string u_ID, int u_IDType, DateTime u_BeginTime, DateTime u_EndTime, int istr, int imax)
+        {
+
+            string ServerIP = ConfigurationManager.AppSettings["ICEServerIP"].Trim();
+            int Port = Int32.Parse( ConfigurationManager.AppSettings["ICEPort"].Trim());
+            ICEAccess ice = new ICEAccess(ServerIP,Port);
+            try
+            {
+                //已修改 furion V30_FURION核心查询需改动 type=1和2时,由t_tran_list改为查询t_order
+                //0买家交易单，9卖家交易单，1，通过交易单查询，2通过给银行订单号查询，4，通过订单查询
+                Q_PAY_LIST cuser = new Q_PAY_LIST(u_ID, u_IDType, u_BeginTime, u_EndTime, istr, imax);
+
+                if (cuser.ICESQL == "")
+                {
+                    if (u_IDType == 0 || u_IDType == 9 || u_IDType == 10 || u_IDType == 13)
+                    {
+                        string fuid = PublicRes.ConvertToFuid(u_ID);
+                        string connstr = PublicRes.GetConnString("t_user_order_bsb", fuid.Substring(fuid.Length - 2));
+                        return cuser.GetResultX_Conn(connstr);
+                    }
+                    else
+                        return cuser.GetResultX("BSB");
+                    //现在要查核心交易单，而不是用订单替代。
+                }
+                else if (u_IDType == 1)
+                {
+                    ice.OpenConn();
+                    string strwhere = "where=" + ICEAccess.URLEncode("flistid=" + u_ID.Trim() + "&");
+                    strwhere += ICEAccess.URLEncode("fcurtype=1&");
+
+                    string strResp = "";
+
+                    //3.0接口测试需要 furion 20090708
+                    DataTable dt = ice.InvokeQuery_GetDataTable(YWSourceType.交易单资源, YWCommandCode.查询交易单信息, u_ID.Trim(), strwhere, out strResp);
+
+                    ice.CloseConn();
+
+                    if (dt == null || dt.Rows.Count == 0)
+                    {
+                        return null;
+                        //throw new LogicException("调用ICE查询T_tran_list无记录" + strResp);
+                    }
+
+                    DataSet ds = new DataSet();
+                    ds.Tables.Add(dt);
+                    return ds;
+                }
+                else
+                {
+                    string errMsg = "";
+                    DataSet ds = CommQuery.GetDataSetFromICE(cuser.ICESQL, CommQuery.QUERY_ORDER, out errMsg);
+
+                    return ds;
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            finally
+            {
+                ice.Dispose();
+            }
+
+        }
+
+        //将银行返回定单号转换成交易单号
+        public string ConvertToListID(string sFbankAcc, DateTime sDateTime)
+        {
+            try
+            {
+                string errMsg = "";
+
+                string strCmd = "bank_acc=" + sFbankAcc + "&query_day=" + sDateTime.ToString("yyyyMMdd");  //增加时间参数 andrew 20110322;
+                string listID = CommQuery.GetOneResultFromICE(strCmd, CommQuery.QUERY_TCBANKROLL_DAY, "Flistid", out errMsg);
+                return listID;
+            }
+            catch
+            {
+                throw new Exception("根据银行订单号和日期查询交易单失败！");
+            }
+        }
+
+        public DataSet GetQueryListDetail(string listid)
+        {
+            try
+            {
+                OrderQueryClassZJ cuser = new OrderQueryClassZJ(listid);
+                string errMsg = "";
+                DataSet ds = CommQuery.GetDataSetFromICE(cuser.ICESQL, CommQuery.QUERY_ORDER, out errMsg);
+                return ds;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("service发生错误,请联系管理员！");
+                return null;
+            }
+        }
+        #endregion
+
+        //查询用户帐户流水表_WithListID
+        public DataSet GetBankRollList_withID(DateTime u_BeginTime, DateTime u_EndTime, string ListID, int istr, int imax)
+        {
+         
+            string strRightCode = "GetBankRollList";
+
+            int sign = 0; string detail, actionType, signStr;
+            actionType = "查询用户帐户流水";
+
+            //PublicRes.CheckUserRight(strUserID,strPassword,strRightCode);
+
+            try
+            {
+                //furion V30_FURION核心查询需改动 等待通用查询接口.  //V30_20090525 资金流水用listid查询 这个要增加多个查询，然后组合结果
+                sign = 1;
+
+
+                Q_BANKROLL_LIST cuser = new Q_BANKROLL_LIST(u_BeginTime, u_EndTime, ListID);
+                //T_BANKROLL_LIST[] tuser = cuser.GetResult();
+                //return tuser;
+                if (cuser.alTables == null || cuser.alTables.Count == 0)
+                    return null;
+
+                string onerow = cuser.alTables[0].ToString().Replace("UID=", "");
+                string strWhere = "uid=" + onerow;
+                strWhere += "&listid=" + ListID;
+
+                DataSet ds;
+                string errMsg;
+
+                if (!CommQuery.GetDataFromICE(strWhere, CommQuery.交易单资金流水_个人, out errMsg, out ds))
+                {
+                    //throw new LogicException(errMsg);
+                }
+
+                bool havefirstds = true;
+                if (ds == null || ds.Tables.Count == 0 || ds.Tables[0] == null || ds.Tables[0].Columns.Count == 0)
+                {
+                    //throw new LogicException("查询买卖家资金流水时有误");
+                    //应该是未支付,啥资金流水都没发生.
+                    //return null;
+                    havefirstds = false;
+                }
+
+                //先取出买卖家的一个数据表
+                for (int i = 1; i < cuser.alTables.Count; i++)
+                {
+                    DataSet newds;
+                    onerow = cuser.alTables[i].ToString();
+                    if (onerow.StartsWith("UID="))
+                    {
+                        strWhere = "uid=" + onerow.Replace("UID=", "");
+                        strWhere += "&listid=" + ListID;
+
+                        if (!CommQuery.GetDataFromICE(strWhere, CommQuery.交易单资金流水_个人, out errMsg, out newds))
+                        {
+                            //throw new LogicException(errMsg);
+                        }
+                    }
+                    else
+                    {
+                        strWhere = "start_time=" + onerow.Replace("TME=", "").Substring(0, 10);
+                        strWhere += "&listid=" + ListID;
+
+                        if (!CommQuery.GetDataFromICE(strWhere, CommQuery.交易单资金流水_商户, out errMsg, out newds))
+                        {
+                            //throw new LogicException(errMsg);
+                        }
+                    }
+
+                    //把这次得到的表内容合并入ds中。
+                    if (newds == null || newds.Tables.Count == 0 || newds.Tables[0] == null || newds.Tables[0].Rows.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    if (havefirstds)
+                    {
+                        foreach (DataRow dr in newds.Tables[0].Rows)
+                        {
+                            ds.Tables[0].Rows.Add(dr.ItemArray);
+                        }
+                    }
+                    else
+                    {
+                        havefirstds = true;
+                        ds = newds;
+                    }
+                }
+
+                //furion 增加银行资金流水表。 20090813
+                for (int i = 1; i < cuser.alTables.Count; i++)
+                {
+                    DataSet newds;
+                    onerow = cuser.alTables[i].ToString();
+                    if (onerow.StartsWith("UID="))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        strWhere = "start_time=" + onerow.Replace("TME=", "").Substring(0, 10);
+                        strWhere += "&listid=" + ListID;
+
+                        if (!CommQuery.GetDataFromICE(strWhere, CommQuery.交易单资金流水_银行, out errMsg, out newds))
+                        {
+                            //throw new LogicException(errMsg);
+                        }
+                    }
+
+                    //把这次得到的表内容合并入ds中。
+                    if (newds == null || newds.Tables.Count == 0 || newds.Tables[0] == null || newds.Tables[0].Rows.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    if (havefirstds)
+                    {
+                        foreach (DataRow dr in newds.Tables[0].Rows)
+                        {
+                            ds.Tables[0].Rows.Add(dr.ItemArray);
+                        }
+                    }
+                    else
+                    {
+                        havefirstds = true;
+                        ds = newds;
+                    }
+                }
+
+                if (ds == null || ds.Tables.Count == 0 || ds.Tables[0] == null || ds.Tables[0].Columns.Count == 0)
+                {
+                    return null;
+                }
+
+                ds.Tables[0].DefaultView.Sort = "Faction_Type DESC";
+                return ds;
+            }
+            catch (Exception e)
+            {
+                sign = 0;
+                throw new Exception("service发生错误,请联系管理员！" + e.Message);
+                return null;
+            }
+            finally
+            {
+                
             }
         }
     }
---
+++ src/KF_SERVICE/CSAPI/TradeService.asmx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using System.Xml.Serialization;
+using CFT.CSOMS.Service.CSAPI.Utility;
+using System.IO;
+using CFT.CSOMS.Service.CSAPI.Language;
+using System.Collections;
+using CFT.CSOMS.Service.CSAPI.BaseInfo;
+using System.Collections.Specialized;
+
+namespace CSAPI
+{
+    /// <summary>
+    /// Summary description for TradeService
+    /// </summary>
+    [WebService(Namespace = "")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class TradeService : System.Web.Services.WebService
+    {
+        [WebMethod(Description="交易记录查询")]
+        public void GetTradeList()
+        {
+            try
+            {
+                Dictionary<string, string> paramsHt = APIUtil.GetQueryStrings();
+                //验证必填参数
+                APIUtil.ValidateParamsNew(paramsHt, "appid", "tradeid", "type", "time", "token");
+                //验证token
+                APIUtil.ValidateToken(paramsHt);
+
+                string tradeid = paramsHt.ContainsKey("tradeid") ? paramsHt["tradeid"].ToString() : "";
+                int type = APIUtil.StringToInt(paramsHt["type"].ToString());
+                DateTime time = APIUtil.StrToDate(paramsHt["time"].ToString());
+
+                var infos = new CFT.CSOMS.BLL.TradeModule.TradeService().GetTradeList(tradeid, type, time);
+                if (infos == null || infos.Tables.Count == 0 || infos.Tables[0].Rows.Count == 0)
+                { 
+                    throw new ServiceException(APIUtil.ERR_NORECORD,ErroMessage.MESSAGE_NORECORD);
+                }
+                List<TradeInfo.TradePayList> list = APIUtil.ConvertTo<TradeInfo.TradePayList>(infos.Tables[0]);
+                APIUtil.Print<TradeInfo.TradePayList>(list);
+            }
+            catch (ServiceException se)
+            {
+                SunLibrary.LoggerFactory.Get("GetTradeList").ErrorFormat("return_code:{0},msg:{1}",se.GetRetcode,se.GetRetmsg);
+                APIUtil.PrintError(se.GetRetcode, se.GetRetmsg);
+            }
+            catch (Exception ex)
+            { 
+                SunLibrary.LoggerFactory.Get("GetTradeList").ErrorFormat("return_code:{0},msg:{1}",APIUtil.ERR_SYSTEM,ex.Message);
+                APIUtil.PrintError(APIUtil.ERR_SYSTEM, ErroMessage.MESSAGE_ERROBUSINESS);
+            }
+        }
+    }
+}
