8f2685bdd34627cc0f2f4f97b2a64265cd50c58f
darrenran <darrenran@tencent.com>
2015-05-15 16:36:04 +0800

    修改完善了--客服系统需求235 的一些功能

--- src/CFT.CSOMS.BLL/TravelModule/AirTickets.cs
+++ src/CFT.CSOMS.BLL/TravelModule/AirTickets.cs
@@ -13,7 +13,6 @@ namespace CFT.CSOMS.BLL.TravelModule
     {
         private DAL.TravelModule.AirTickets Dal = new DAL.TravelModule.AirTickets();
         private static Dictionary<string, int> query_typeInfo;
-        private static Dictionary<string, string> SpInfo;
         static AirTickets()
         {
             query_typeInfo = new Dictionary<string, int>();
@@ -26,20 +25,6 @@ namespace CFT.CSOMS.BLL.TravelModule
             query_typeInfo.Add("TextPassenger_name", 7);
             query_typeInfo.Add("TextMobile", 8);
             query_typeInfo.Add("TextName", 9);
-
-
-
-            SpInfo = new Dictionary<string, string>();
-            SpInfo.Add("qunar", "去哪儿网");
-            SpInfo.Add("csair", "南航直销专区");
-            SpInfo.Add("ceair", "东航直销专区");
-            SpInfo.Add("hnair", "海航直销专区");
-            SpInfo.Add("szair", "深航直销专区");
-            SpInfo.Add("scair", "川航直销专区");
-
-
-            SpInfo.Add("gh_ntn", "黄金假日");
-            SpInfo.Add("51book_ntn", "财付通专区");
         }
 
         /// <summary>
@@ -57,20 +42,13 @@ namespace CFT.CSOMS.BLL.TravelModule
         /// <returns></returns>
         public DataSet AirTicketsOrderQuery(int query_type, string wd, string trade_type, string uin, DateTime start_time, DateTime end_time, string sp_code, int limit, int page_id = 1)
         {
-            var answer = Dal.AirTicketsOrderQuery(query_type, wd, trade_type, uin, start_time, end_time, sp_code, limit, page_id);
-            string msg;
-            if (answer.IndexOf("<ret_num>0</ret_num>") == -1)
+            var ds = Dal.AirTicketsOrderQuery(query_type, wd, trade_type, uin, start_time, end_time, sp_code, limit, page_id);
+            if (ds != null && ds.Tables != null)
             {
-                var ds = CommQuery.PaseCgiXmlForTravelPlatform(answer, out msg);
-                var dts = AnalyzeXmlPersonsByString(answer, out msg);
-                if (dts != null)
-                {
-                    ds.Tables.AddRange(dts);
-                }
-                FieldMapString(ds);
-                return ds;
+               FenToYuan_Table(ds.Tables[0], "_str", "total_money", "adult_airport_tax", "adult_fuel_tax", "child_airport_tax", "child_fuel_tax", "adult_price", "child_price", "airport_tax_money", "fuel_tax_money", "ticket_money", "insurance_money");
             }
-            return null;
+            return ds;
+
         }
 
         /// <summary>
@@ -98,154 +76,35 @@ namespace CFT.CSOMS.BLL.TravelModule
         }
 
         /// <summary>
-        /// 获取订单状态文本
+        /// RMB分转元
         /// </summary>
-        /// <param name="strTrade_state"></param>
-        /// <returns></returns>
-        public string GetTradeState(string trade_state)
+        /// <param name="dt">表对象</param>
+        /// <param name="suffix">元字段加入的后缀</param>
+        /// <param name="column">要转换的分字段</param>
+        public void FenToYuan_Table(DataTable dt, string suffix, params string[] column)
         {
-            switch (trade_state)
+            var columns = new DataColumn[column.Length];
+            for (int i = 0; i < column.Length; i++)
             {
-                case "1":
-                case "2": return "创建订单";
-                case "3": return "占座未支付";
-                case "4": return "申请支付";
-                case "5":
-                case "6": return "支付成功";
-                case "7": return "出票异常";
-                case "8":
-                case "12":
-                case "13": return "出票成功";
-                case "14":
-                case "16": return "部分退票中";
-                case "17": return "部分退票成功";
-                case "18": return "部分退票失败";
-                case "19":
-                case "21": return "全部退票中";
-                case "22": return "全部退票成功";
-                case "23": return "全部退票失败";
-                case "11": return "出票失败";
-                case "99": return "抢购成功";
-                default: return "未知";
+                var col = column[i];
+                if (!dt.Columns.Contains(col))
+                {
+                    throw new Exception("dt中不存在 '" + col + " '这个字段");
+                }
+                columns[i] = new DataColumn(col + suffix, typeof(string));
+            }
+            dt.Columns.AddRange(columns);
+            foreach (DataRow item in dt.Rows)
+            {
+                item.BeginEdit();
+                foreach (var cName in column)
+                {
+                    var fen = item[cName].ToString();
+                    item[cName + suffix] = TENCENT.OSS.CFT.KF.Common.MoneyTransfer.FenToYuan(fen);
+                }
+                item.EndEdit();
             }
         }
 
-        /// <summary>
-        /// 解析XML 中 passengers|contact 两个节点
-        /// </summary>
-        /// <param name="answer"></param>
-        /// <param name="msg"></param>
-        /// <returns></returns>
-        private DataTable[] AnalyzeXmlPersonsByString(string answer, out string msg)
-        {
-            msg = "";
-            try
-            {
-                XmlDocument doc = new XmlDocument();
-                doc.LoadXml(answer);
-                XmlElement root = doc.DocumentElement;
-                DataTable passengers = new DataTable("passengers");
-                DataTable contact = new DataTable("contact");
-                var strType = typeof(string);
-                var records = root.SelectSingleNode("records");
-
-                passengers.Columns.Add("listid", strType);
-                var passengersTitles = records.FirstChild.SelectSingleNode("passengers").FirstChild.Attributes.Cast<XmlAttribute>().Select(u => new DataColumn(u.Name, strType)).ToArray();
-                passengers.Columns.AddRange(passengersTitles);
-
-                contact.Columns.Add("listid", strType);
-                var contactTitles = records.FirstChild.SelectSingleNode("contact").Attributes.Cast<XmlAttribute>().Select(u => new DataColumn(u.Name, strType)).ToArray();
-                contact.Columns.AddRange(contactTitles);
-                contact.Columns.Add("mailtype");
-
-                foreach (XmlNode item in records)
-                {
-                    var listid = item.SelectSingleNode("listinfo").Attributes.GetNamedItem("listid").Value;
-                    foreach (XmlNode passenger in item.SelectSingleNode("passengers").ChildNodes)
-                    {
-                        var pRow = passengers.NewRow();
-                        foreach (XmlAttribute attr in passenger.Attributes)
-                        {
-                            pRow[attr.Name] = attr.Value;
-                        }
-                        pRow["listid"] = listid;
-                        passengers.Rows.Add(pRow);
-                    }
-                    var cRow = contact.NewRow();
-                    foreach (XmlAttribute attr in item.SelectSingleNode("contact").Attributes)
-                    {
-                        cRow[attr.Name] = attr.Value;
-                    }
-
-                    var xcd = item.SelectSingleNode("xcdexpress");
-                    if (xcd != null)
-                    {
-                        cRow["mailtype"] = xcd.Attributes["mailtype"].Value ?? "";
-                    }
-                    cRow["listid"] = listid;
-                    contact.Rows.Add(cRow);
-                }
-
-                return new DataTable[] { passengers, contact };
-            }
-            catch (Exception ex)
-            {
-                msg = "解析XML,passengers|contact 两个节点出错" + ex.ToString();
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// 把DataSet中的一些代码字段映射成文本
-        /// </summary>
-        /// <param name="ds"></param>
-        private void FieldMapString(DataSet ds)
-        {
-            var pDT = ds.Tables["passengers"];
-            if (pDT != null && pDT.Rows.Count > 0)
-            {
-                pDT.Columns.Add("cert_type_str", typeof(string));
-                pDT.Columns.Add("type_str", typeof(string));
-                foreach (DataRow item in pDT.Rows)
-                {
-                    var certType = item["cert_type"].ToString();
-                    var _type = item["type"].ToString();
-                    item.BeginEdit();
-                    item["cert_type_str"] = certType == "NI" ? "身份证" : certType == "PP" ? "护照" : "其他";//OT  其他
-                    item["type_str"] = _type == "AD" ? "成人" : "儿童";  //CH  儿童
-                    item.EndEdit();
-                }
-            }
-
-            var DT0 = ds.Tables[0];
-            if (DT0 != null && DT0.Rows.Count > 0)
-            {
-                DT0.Columns.Add("insur_com_str", typeof(string));
-                DT0.Columns.Add("strSp_code", typeof(string));
-                DT0.Columns.Add("strTrade_state", typeof(string));
-                DT0.Columns.Add("trip_flag_str", typeof(string));
-
-                DT0.Columns.Add("adult_total_money", typeof(string));
-                DT0.Columns.Add("child_total_money", typeof(string));
-                foreach (DataRow item in DT0.Rows)
-                {
-                    var company = item["insur_com"].ToString();
-                    var sp_code = item["sp_code"].ToString();
-                    var trade_state = item["trade_state"].ToString();
-                    var trip_flag = item["trip_flag_str"].ToString();
-                    item.BeginEdit();
-                    item["insur_com_str"] = company == "hezhong" ? "合众" : "其他";
-                    item["strSp_code"] = SpInfo.ContainsKey(sp_code) ? SpInfo[sp_code] : "未知";
-                    item["strTrade_state"] = GetTradeState(trade_state);
-                    item["trip_flag_str"] = trip_flag == "1" ? "去程" : "回程";
-
-                    //adult_airport_tax     adult_fuel_tax  adult_price
-                    //child_airport_tax     child_fuel_tax  child_price
-                    item["adult_total_money"] = ((Convert.ToDecimal(item["adult_airport_tax"]) + Convert.ToDecimal(item["adult_fuel_tax"]) + Convert.ToDecimal(item["adult_price"])) / 100 * Convert.ToInt32(item["adult_num"])).ToString("f2");
-                    item["child_total_money"] = ((Convert.ToDecimal(item["child_airport_tax"]) + Convert.ToDecimal(item["child_fuel_tax"]) + Convert.ToDecimal(item["child_price"])) / 100 * Convert.ToInt32(item["child_num"])).ToString("f2"); ;
-                    item.EndEdit();
-                }
-            }
-        }
     }
 }
