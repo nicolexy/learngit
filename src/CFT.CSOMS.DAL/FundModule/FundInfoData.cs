6d6026be67179e49eeb1998477374303e0f38b7f
echoliao <echoliao@tencent.com>
2015-06-01 18:42:12 +0800

    “查询用户资金流水情况”中新增“提现单号”字段 修复

--- src/CFT.CSOMS.DAL/FundModule/FundInfoData.cs
+++ src/CFT.CSOMS.DAL/FundModule/FundInfoData.cs
@@ -68,7 +68,7 @@ namespace CFT.CSOMS.DAL.FundModule
             {
                 da.OpenConn();
                 var table_name = string.Format("fund_db_{0}.t_trade_fund_{1}", listid.Substring(listid.Length - 2), listid.Substring(listid.Length - 3, 1));
-                string Sql = string.Format(" select Fspid,Ffund_name,Ffund_code,Fpur_type,Fcharge_fee from {0} where Fspid='{1}' and Flistid='{2}'", table_name, spid, listid);
+                string Sql = string.Format(" select Ffetchid, Fspid,Ffund_name,Ffund_code,Fpur_type,Fcharge_fee from {0} where Fspid='{1}' and Flistid='{2}'", table_name, spid, listid);
                 DataSet ds = da.dsGetTotalData(Sql);
 
                 return ds.Tables[0];
