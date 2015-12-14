0ba3c43ff223ff92e1593a8afa588370ff4db5bf
v_yweihuang <v_yweihuang@tencent.com>
2015-12-14 10:53:35 +0800

    客服系统需求301-账户多set调用核心的接口改造 http://tapd.oa.com/tenpay_kf/prong/stories/view/1010068761056968787

--- src/CFT.CSOMS.DAL.Tests/CFTAccountTest/AccountDataTest.cs
+++ src/CFT.CSOMS.DAL.Tests/CFTAccountTest/AccountDataTest.cs
@@ -24,10 +24,63 @@ namespace CFT.CSOMS.DAL.Tests.CFTAccountTest
                 req += "&uid=298684980";
                 DataSet userDs = CommQuery.GetDSForServiceFromICE(req, "qry_user_balance_slave_c", false, out errMsg);
                 //YWCommandCode.查询用户信息
+                GetUserInfoByCurType("1");
+                GetUserInfoByCurType("80");
             }
             catch (Exception ex)
             {
             }
+        }
+
+        public DataTable GetUserInfoByCurType(string fcurtype)
+        {
+            string errMsg = "";
+            DataTable dt = new DataTable();
+
+            string req = "MSG_NO=" + DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            req += "&uid=298684980";
+            if (fcurtype == "1")
+            {
+                req += "&curtype=" + fcurtype;
+                DataSet userDs = CommQuery.GetDSForServiceFromICE(req, "qry_user_balance_slave_c", false, out errMsg);
+                if (userDs != null && userDs.Tables.Count > 0 && userDs.Tables[0] != null && userDs.Tables[0].Columns.Count > 0)
+                {
+                    dt = userDs.Tables[0].Copy();
+                    #region
+                    dt.Columns["uid"].ColumnName = "Fuid";
+                    dt.Columns["curtype"].ColumnName = "Fcurtype";
+                    dt.Columns["balance"].ColumnName = "Fbalance";
+                    dt.Columns["con"].ColumnName = "Fcon";
+                    dt.Columns["state"].ColumnName = "Fstate";
+                    dt.Columns["standby1"].ColumnName = "Fstandby1";
+                    dt.Columns["standby2"].ColumnName = "Fstandby2";
+                    dt.Columns["modify_time"].ColumnName = "Fmodify_time";
+                    dt.Columns["user_type"].ColumnName = "Fuser_type";
+                    dt.Columns["qqid"].ColumnName = "Fqqid";
+                    dt.Columns["truename"].ColumnName = "Ftruename";
+                    dt.Columns["company_name"].ColumnName = "Fcompany_name";
+                    dt.Columns["login_ip"].ColumnName = "Flogin_ip";
+                    dt.Columns["memo"].ColumnName = "Fmemo";
+                    dt.Columns["create_time"].ColumnName = "Fcreate_time";
+                    dt.Columns["bpay_state"].ColumnName = "Fbpay_state";
+                    #endregion
+                }
+            }
+            else // 子账户余额查询
+            {
+                req += "&curtype_list=" + fcurtype;
+                DataSet userDs = CommQuery.GetDSForServiceFromICE(req, "qry_subuser_balance_c", false, out errMsg);
+                if (userDs != null && userDs.Tables.Count > 0 && userDs.Tables[0] != null && userDs.Tables[0].Columns.Count > 0)
+                {
+                    dt = userDs.Tables[0].Copy();
+                    #region
+                    dt.Columns["Fbalance_"+fcurtype].ColumnName = "Fbalance";
+                    dt.Columns["Fcon_" + fcurtype].ColumnName = "Fcon";
+                    dt.Columns["Fstate_" + fcurtype].ColumnName = "Fstate";
+                    #endregion
+                }
+            }
+            return dt;
         }
     }
 }
