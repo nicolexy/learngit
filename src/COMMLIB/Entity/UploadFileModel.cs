3c88b9f11bca6d320b35a1908e055cdcce75bc5d
v_kyaoliu <v_kyaoliu@tencent.com>
2015-12-27 19:36:59 +0800

    理财通的 基金强赎 ,理财通余额强赎 接入 FPS 文件服务

---
+++ src/COMMLIB/Entity/UploadFileModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace commLib.Entity
+{
+    /// <summary>
+    /// 文件上传返回值类型
+    /// </summary>
+    public class UploadFileModel
+    {
+        /// <summary>
+        ///    状态码： 0表示响应成功。其它查看文档
+        /// </summary>
+        public int errcode { get; set; }
+        /// <summary>
+        /// 详细消息
+        /// </summary>
+        public string msg { get; set; }
+        public string fileid { get; set; }
+        public string fileurl { get; set; }
+        public string filemd5 { get; set; }
+
+        [System.Web.Script.Serialization.ScriptIgnore]
+        public string url { get; set; }
+    }
+}
