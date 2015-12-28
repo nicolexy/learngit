4eacc70211f5a501bda6641389d827ef5c73b614
v_yqyqguo <v_yqyqguo@tencent.com>
2015-12-28 20:21:07 +0800

    20151210客服系统BUG-代扣限额查询无法查询 http://tapd.oa.com/tenpay_kf/prong/stories/view/1010068761057260867

---
+++ src/CFT.CSOMS.DAL/DKModule/DKData.cs
@@ -0,0 +1,126 @@
+﻿using CFT.CSOMS.DAL.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using TENCENT.OSS.C2C.Finance.Common.CommLib;
+using TENCENT.OSS.CFT.KF.DataAccess;
+
+namespace CFT.CSOMS.DAL.DKModule
+{
+    public class DKData
+    {
+        public DataTable GetDKBankList()
+        {
+            var ip = Apollo.Common.Configuration.AppSettings.Get<string>("DKRelay_IP", "10.123.6.291");
+            var port = Apollo.Common.Configuration.AppSettings.Get<int>("DKRelay_Port", 356001);
+            string requestString = "banktype_filter_rule=1&biz_type=1";
+            string answer = RelayAccessFactory.RelayInvoke(requestString, "6891", false, false, ip, port, "");
+            string msg;
+            DataTable  dt = CommQuery.ParseRelayPageMethod2(answer, out msg);
+           
+            if (!string.IsNullOrEmpty(msg))
+            {
+                throw new Exception(msg);
+            }
+            return dt;
+        }
+
+        //银行账户限额批次查询函数
+        public DataTable GetDKLimit_List(string bank_sname, string bankaccno, string servicecode, int limStart, int limMax)
+        {
+
+            DataSet ds = new DataSet();
+            MySqlAccess da = new MySqlAccess(PublicRes.GetConnString("INC_NEW"));
+            try
+            {
+                da.OpenConn();
+                bankaccno = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(bankaccno, "md5").ToLower();
+                string roleid = bank_sname + bankaccno + "@cep.tenpay.com";
+                roleid = PublicRes.ConvertToFuid(roleid);
+                roleid = "2000000501";
+                string strSql = " select Fservice_code,'" + bank_sname + "' as Fbanktype,'" + bankaccno + "' as Fbankaccno, ";
+
+                strSql += "sum(case Fdimension when 1 then Fonce_data else 0 end)/100 as Fonce_data,";//单笔限额
+
+                strSql += "sum(case Fdimension when 1 then Fday_data else 0 end)/100 as Fday_sum_data,";//单日限额
+                strSql += "sum(case Fdimension when 3 then Fday_data else 0 end)/100 as Fday_use_data,";//单日累计使用限额
+
+                strSql += "sum(case Fdimension when 1 then Fweek_data else 0 end)/100 as Fweek_sum_data,";//单周限额
+                strSql += "sum(case Fdimension when 3 then Fweek_data else 0 end)/100 as Fweek_use_data,";//单周累计使用限额
+
+                strSql += "sum(case Fdimension when 1 then Fmounth_data else 0 end)/100 as Fmonth_sum_data,";//单月限额
+                strSql += "sum(case Fdimension when 3 then Fmounth_data else 0 end)/100 as Fmonth_use_data,";//单月累计使用限额
+
+                strSql += "sum(case Fdimension when 1 then Fquarter_data else 0 end)/100 as Fquarter_sum_data,";//单季限额
+                strSql += "sum(case Fdimension when 3 then Fquarter_data else 0 end)/100 as Fquarter_use_data,";//单季累计使用限额
+
+                strSql += "sum(case Fdimension when 1 then Fyear_data else 0 end)/100 as Fyear_sum_data,";//单年限额
+                strSql += "sum(case Fdimension when 3 then Fyear_data else 0 end)/100 as Fyear_use_data,";//单年累计使用限额
+
+                strSql += "sum(case Fdimension when 2 then Fday_data else 0 end) as Fday_sum_count,";//单日限次
+                strSql += "sum(case Fdimension when 4 then Fday_data else 0 end) as Fday_use_count,";//单日累计使用次数
+
+                strSql += "sum(case Fdimension when 2 then Fweek_data else 0 end) as Fweek_sum_count,";//单周限次
+                strSql += "sum(case Fdimension when 4 then Fweek_data else 0 end) as Fweek_use_count,";//单周累计使用次数
+
+                strSql += "sum(case Fdimension when 2 then Fmounth_data else 0 end) as Fmonth_sum_count,";//单月限次
+                strSql += "sum(case Fdimension when 4 then Fmounth_data else 0 end) as Fmonth_use_count,";//单月累计使用次数
+
+                strSql += "sum(case Fdimension when 2 then Fquarter_data else 0 end) as Fquarter_sum_count,";//单季限次
+                strSql += "sum(case Fdimension when 4 then Fquarter_data else 0 end) as Fquarter_use_count,";//单季累计使用次数
+
+                strSql += "sum(case Fdimension when 2 then Fyear_data else 0 end) as Fyear_sum_count,";//单年限次
+                strSql += "sum(case Fdimension when 4 then Fyear_data else 0 end) as Fyear_use_count ";//单年累计使用次数
+
+                strSql += " from " + PublicRes.GetTName("cft_cep_db", "t_service_limit", roleid) + " where Frole_id='" + roleid + "' ";
+
+                strSql += string.IsNullOrEmpty(servicecode) ? "" : " and Fservice_code='" + servicecode + "'";
+
+                strSql += " and Fflag=1 group by Fservice_code ";
+
+                ds = da.dsGetTableByRange(strSql, limStart, limMax);
+
+                if (ds == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
+                    return null;
+
+                ds.Tables[0].Columns.Add("Fspid", typeof(string));
+                ds.Tables[0].Columns.Add("Fspname", typeof(string));
+                ds.Tables[0].Columns.Add("Fcodeid", typeof(string));
+                ds.Tables[0].Columns.Add("Fcodename", typeof(string));
+
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    string tmp = dr["Fservice_code"].ToString();
+                    string spid = tmp.Substring(0, 10);
+                    dr["Fspid"] = spid;
+
+                    string msg = "";
+                    dr["Fspname"] = CommQuery.GetOneResultFromICE("spid=" + spid, CommQuery.QUERY_MERCHANTINFO, "FName", out msg);
+                    string codeid = dr["Fservice_code"].ToString().Replace(spid, "");
+                    dr["Fcodeid"] = codeid;
+
+                    if (getData.htService_code.Contains(codeid))
+                    {
+                        dr["Fcodename"] = getData.htService_code[codeid].ToString();
+                    }
+                    else
+                    {
+                        dr["Fcodename"] = "";
+                    }
+                }
+
+                return ds.Tables[0];
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                da.Dispose();
+            }
+        }
+    }
+}
--- src/KF_WEB/NewQueryInfoPages/QueryDKLimitPage.aspx.cs
+++ src/KF_WEB/NewQueryInfoPages/QueryDKLimitPage.aspx.cs
@@ -11,139 +11,157 @@ using System.Web.UI.HtmlControls;
 
 using TENCENT.OSS.CFT.KF.KF_Web.classLibrary;
 using Tencent.DotNet.Common.UI;
+using CFT.CSOMS.BLL.DKModule;
 
 namespace TENCENT.OSS.CFT.KF.KF_Web.NewQueryInfoPages
 {
-	/// <summary>
-	/// QueryDKLimitPage 的摘要说明。
-	/// </summary>
-	public partial class QueryDKLimitPage : System.Web.UI.Page
-	{
-	
-		protected void Page_Load(object sender, System.EventArgs e)
-		{
-			// 在此处放置用户代码以初始化页面
-			if(!IsPostBack)
-			{
-				classLibrary.setConfig.GetAllBankList(ddlBank_Type);
+    /// <summary>
+    /// QueryDKLimitPage 的摘要说明。
+    /// </summary>
+    public partial class QueryDKLimitPage : System.Web.UI.Page
+    {
 
-				//tbx_bankID.Text = "6222350040762419";
-				//ddlBank_Type.Items.Add(new ListItem("测试","3007"));
-			}
-		}
+        protected void Page_Load(object sender, System.EventArgs e)
+        {
+            // 在此处放置用户代码以初始化页面
+            if (!IsPostBack)
+            {
+                classLibrary.setConfig.GetAllBankList_DK(ddlBank_Type);
 
-		#region Web 窗体设计器生成的代码
-		override protected void OnInit(EventArgs e)
-		{
-			//
-			// CODEGEN: 该调用是 ASP.NET Web 窗体设计器所必需的。
-			//
-			InitializeComponent();
-			base.OnInit(e);
-		}
-		
-		/// <summary>
-		/// 设计器支持所需的方法 - 不要使用代码编辑器修改
-		/// 此方法的内容。
-		/// </summary>
-		private void InitializeComponent()
-		{    
-			this.DataGrid_QueryResult.ItemCommand += new System.Web.UI.WebControls.DataGridCommandEventHandler(this.DataGrid_QueryResult_ItemCommand);
-			this.pager.PageChanged += new Wuqi.Webdiyer.PageChangedEventHandler(this.pager_PageChanged);
+                //tbx_bankID.Text = "6222350040762419";
+                //ddlBank_Type.Items.Add(new ListItem("测试","3007"));
+            }
+        }
 
-		}
-		#endregion
+        #region Web 窗体设计器生成的代码
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: 该调用是 ASP.NET Web 窗体设计器所必需的。
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
 
-		protected void btn_serach_Click(object sender, System.EventArgs e)
-		{
-			if(tbx_bankID.Text.Trim() == "")
-				return;
-			if(ddlBank_Type.SelectedValue == null || ddlBank_Type.SelectedValue == "")
-				return;
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要使用代码编辑器修改
+        /// 此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGrid_QueryResult.ItemCommand += new System.Web.UI.WebControls.DataGridCommandEventHandler(this.DataGrid_QueryResult_ItemCommand);
+            this.pager.PageChanged += new Wuqi.Webdiyer.PageChangedEventHandler(this.pager_PageChanged);
 
-			ViewState["bankaccno"] = tbx_bankID.Text.Trim();
-			ViewState["banktype"] = ddlBank_Type.SelectedValue.Trim();
+        }
+        #endregion
 
-			BindData(1);
-		}
+        protected void btn_serach_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (tbx_bankID.Text.Trim() == "")
+                    return;
+                if (ddlBank_Type.SelectedValue == null || ddlBank_Type.SelectedValue == "")
+                    return;
 
-		private void BindData(int index)
-		{
-			string bankaccno = ViewState["bankaccno"].ToString();
-			string banktype = ViewState["banktype"].ToString();
+                ViewState["bankaccno"] = tbx_bankID.Text.Trim();
+                ViewState["banktype"] = ddlBank_Type.SelectedValue.Trim();
 
-			Query_Service.Query_Service qs = new TENCENT.OSS.CFT.KF.KF_Web.Query_Service.Query_Service();
-			qs.Finance_HeaderValue = classLibrary.setConfig.setFH(this);
+                BindData(1);
+            }
+            catch (Exception eSys)
+            {
+                WebUtils.ShowMessage(this.Page, "读取数据失败！" + HttpUtility.JavaScriptStringEncode(eSys.Message.ToString()));
+            }
+        }
 
-			DataSet ds = qs.GetDKLimit_List(banktype,bankaccno,(index - 1) * this.pager.PageSize,this.pager.PageSize);
-			if(ds == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
-			{
-				WebUtils.ShowMessage(this,"查询结果为空");
-				return;
-			}
+        private void BindData(int index)
+        {
+            string bankaccno = ViewState["bankaccno"].ToString();
+            string banktype = ViewState["banktype"].ToString();
 
-			DataGrid_QueryResult.DataSource = ds.Tables[0].DefaultView;
-			DataGrid_QueryResult.DataBind();
-		}
+            //Query_Service.Query_Service qs = new TENCENT.OSS.CFT.KF.KF_Web.Query_Service.Query_Service();
+            //qs.Finance_HeaderValue = classLibrary.setConfig.setFH(this);
 
-		private void pager_PageChanged(object src, Wuqi.Webdiyer.PageChangedEventArgs e)
-		{
-			int index = e.NewPageIndex;
-			BindData(index);
-		}
+            //DataSet ds = qs.GetDKLimit_List(banktype,bankaccno,(index - 1) * this.pager.PageSize,this.pager.PageSize);
+            DataTable dt = new DKService().GetDKLimit_List(banktype, bankaccno, "", (index - 1) * this.pager.PageSize, this.pager.PageSize);
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                WebUtils.ShowMessage(this, "查询结果为空");
+                return;
+            }
 
-		private void DataGrid_QueryResult_ItemCommand(object source, System.Web.UI.WebControls.DataGridCommandEventArgs e)
-		{
-			if(e.CommandName == "detail")
-			{
-				string banktype = e.Item.Cells[1].Text;
-				string bankaccno = e.Item.Cells[2].Text;
-				string servicecode = e.Item.Cells[3].Text;
+            DataGrid_QueryResult.DataSource = dt.DefaultView;
+            DataGrid_QueryResult.DataBind();
+        }
 
-				Query_Service.Query_Service qs = new TENCENT.OSS.CFT.KF.KF_Web.Query_Service.Query_Service();
-				qs.Finance_HeaderValue = classLibrary.setConfig.setFH(this);
+        private void pager_PageChanged(object src, Wuqi.Webdiyer.PageChangedEventArgs e)
+        {
+            int index = e.NewPageIndex;
+            BindData(index);
+        }
 
-				DataSet ds = qs.GetDKLimit_Detail(banktype,bankaccno,servicecode);
-				if(ds == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
-				{
-					return;
-				}
-				else
-				{
-					DataRow dr = ds.Tables[0].Rows[0];
+        private void DataGrid_QueryResult_ItemCommand(object source, System.Web.UI.WebControls.DataGridCommandEventArgs e)
+        {
+            if (e.CommandName == "detail")
+            {
+                try
+                {
+                    string banktype = e.Item.Cells[1].Text;
+                    string bankaccno = e.Item.Cells[2].Text;
+                    string servicecode = e.Item.Cells[3].Text;
 
-					lb_c1.Text = dr["Fonce_data"].ToString();
-					lb_c3.Text = dr["Fday_sum_data"].ToString();
-					lb_c4.Text = dr["Fday_use_data"].ToString();
+                    //Query_Service.Query_Service qs = new TENCENT.OSS.CFT.KF.KF_Web.Query_Service.Query_Service();
+                    //qs.Finance_HeaderValue = classLibrary.setConfig.setFH(this);
 
-					lb_c5.Text = dr["Fweek_sum_data"].ToString();
-					lb_c6.Text = dr["Fweek_use_data"].ToString();
+                    //DataSet ds = qs.GetDKLimit_Detail(banktype,bankaccno,servicecode);
+                    DataTable dt = new DKService().GetDKLimit_List(banktype, bankaccno, servicecode, 0, 1);
 
-					lb_c7.Text = dr["Fmonth_sum_data"].ToString();
-					lb_c8.Text = dr["Fmonth_use_data"].ToString();
+                    if (dt == null || dt.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        DataRow dr = dt.Rows[0];
 
-					lb_c9.Text = dr["Fquarter_sum_data"].ToString();
-					lb_c10.Text = dr["Fquarter_use_data"].ToString();
+                        lb_c1.Text = dr["Fonce_data"].ToString();
+                        lb_c3.Text = dr["Fday_sum_data"].ToString();
+                        lb_c4.Text = dr["Fday_use_data"].ToString();
 
-					lb_c11.Text = dr["Fyear_sum_data"].ToString();
-					lb_c12.Text = dr["Fyear_use_data"].ToString();
+                        lb_c5.Text = dr["Fweek_sum_data"].ToString();
+                        lb_c6.Text = dr["Fweek_use_data"].ToString();
 
-					lb_c13.Text = dr["Fday_sum_count"].ToString();
-					lb_c14.Text = dr["Fday_use_count"].ToString();
+                        lb_c7.Text = dr["Fmonth_sum_data"].ToString();
+                        lb_c8.Text = dr["Fmonth_use_data"].ToString();
 
-					lb_c15.Text = dr["Fweek_sum_count"].ToString();
-					lb_c16.Text = dr["Fweek_use_count"].ToString();
+                        lb_c9.Text = dr["Fquarter_sum_data"].ToString();
+                        lb_c10.Text = dr["Fquarter_use_data"].ToString();
 
-					lb_c17.Text = dr["Fmonth_sum_count"].ToString();
-					lb_c18.Text = dr["Fmonth_use_count"].ToString();
+                        lb_c11.Text = dr["Fyear_sum_data"].ToString();
+                        lb_c12.Text = dr["Fyear_use_data"].ToString();
 
-					lb_c19.Text = dr["Fquarter_sum_count"].ToString();
-					lb_c20.Text = dr["Fquarter_use_count"].ToString();
+                        lb_c13.Text = dr["Fday_sum_count"].ToString();
+                        lb_c14.Text = dr["Fday_use_count"].ToString();
 
-					lb_c23.Text = dr["Fyear_sum_count"].ToString();
-					lb_c24.Text = dr["Fyear_use_count"].ToString();
-				}
-			}
-		}
-	}
+                        lb_c15.Text = dr["Fweek_sum_count"].ToString();
+                        lb_c16.Text = dr["Fweek_use_count"].ToString();
+
+                        lb_c17.Text = dr["Fmonth_sum_count"].ToString();
+                        lb_c18.Text = dr["Fmonth_use_count"].ToString();
+
+                        lb_c19.Text = dr["Fquarter_sum_count"].ToString();
+                        lb_c20.Text = dr["Fquarter_use_count"].ToString();
+
+                        lb_c23.Text = dr["Fyear_sum_count"].ToString();
+                        lb_c24.Text = dr["Fyear_use_count"].ToString();
+                    }
+                }
+                catch (Exception eSys)
+                {
+                    WebUtils.ShowMessage(this.Page, "读取数据失败！" + HttpUtility.JavaScriptStringEncode(eSys.Message.ToString()));
+                }
+            }
+        }
+    }
 }
