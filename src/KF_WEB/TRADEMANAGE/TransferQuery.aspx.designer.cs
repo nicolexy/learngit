eba0d02cd7b67fed04518a4b43f1aeeceb8ac3eb
v_hjlong <v_hjlong@tencent.com>
2016-07-13 15:14:05 +0800

    页面调整

--- src/KF_WEB/TRADEMANAGE/TransferQuery.aspx.designer.cs
+++ src/KF_WEB/TRADEMANAGE/TransferQuery.aspx.designer.cs
@@ -85,6 +85,15 @@ namespace TENCENT.OSS.CFT.KF.KF_Web.TradeManage {
         protected global::System.Web.UI.WebControls.TextBox txt_CaiFuTongAccount;
         
         /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
         /// tab_transfer_info control.
         /// </summary>
         /// <remarks>
