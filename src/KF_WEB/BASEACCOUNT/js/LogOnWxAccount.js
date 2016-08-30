24cee004fa7853c757aa142fa80a420504f05d8c
v_shideyang <v_shideyang@tencent.com>
2016-08-30 11:16:15 +0800

    代码更正

--- src/KF_WEB/BASEACCOUNT/js/LogOnWxAccount.js
+++ src/KF_WEB/BASEACCOUNT/js/LogOnWxAccount.js
@@ -2,7 +2,7 @@
     var rewxid= $("#txt_re_wxid").val().replace(/^\s+|\s+$/g, "");
     if (wxid == undefined || wxid == "" || wxid == null) { $("#error_info").html("微信号不能为空！"); return false; }
     else if (wxid != rewxid) { $("#error_info").html("两次输入不一致，请重新输入！"); return false; }
-    else if (reason == undefined || reason == "" || reason == null) { $("#error_info").html("理由不能为空！"); return false; }    
+    else if (reason == undefined || reason == "" || reason == null) { $("#error_info").html("销户原因不能为空！"); return false; }
     else { return true; }
 }
 function ajaxSubmit() {
